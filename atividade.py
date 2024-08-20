b5e714dcb3e2aa2f4fe124faa2c0214e487555bd
iranferreir <iranferreiradossantos123@gmail.com>
2024-08-20 20:02:22 +0000

    atividade

---
+++ atividade.py
@@ -0,0 +1,24 @@
+import os
+
+os.system("cls || cleas")
+
+#Solicitando dades
+nome = input("digite seu nome:")
+idade = int(input)("digite sua idade:")
+peimeira_nota = float(input)("digite sua peimeira_nota:")
+segunda_nota = float(input)("digite sua segunda_nota:")
+terceira_nota = float(input)("digite sua terceira_nota:")
+media = (peimeira_nota + segunda_nota + terceira_nota) / 3
+
+if media <7
+ print("o alumo sera reprovado")
+else: 
+ print("o aluno sera aprovado")
+
+#verificando
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"peimeira_nota: {peimeira_nota}")
+print(f"segunda_nota: {segunda_nota}")
+print(f"terceira_nota: {terceira_nota}")
+print(f"media: {media}")
---
+++ import os.py
@@ -0,0 +1,26 @@
+ import os
+
+ os.system("cls || cleas")
+
+#Solicitando dades
+primeiro_numero = float(input("digite seu primeiro_numero: "))
+segundo_numero = float(input("digite seu segundo_numero: "))
+
+#calculando
+soma = primeiro_numero + segundo_numero
+produto = primeiro_numero * segundo_numero
+media = (primeiro_numero + segundo_numero) /2
+
+if (primeiro_numero > segundo_numero):
+   nemor_numero = segundo_numero
+else:
+   nemor_numero = primeiro_numero
+
+if (segundo_numero > primeiro_numero):
+   maior_numero = segundo_numero
+else:
+   maior_numero = segundo_numero
+
+#exibindo dades
+print(f"primeiro_numero: {primeiro_numero}")
+print(f"segundo_numero: {segundo_numero}")  
