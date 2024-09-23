af705e55ce12bcada4effe9b2c92bb7c2105fd03
iranferreir <iranferreiradossantos123@gmail.com>
2024-09-23 19:59:25 +0000

    atividade

---
+++ teste de versionamente.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls || clear")
+
+print("Olá, mundo!")
+
+# Identificar arquivos para salva (commit)
+# git status
+
+#Adicionar arquivos para salva (commit)
+# git add .
+
+# Realizando salvamento (commit)
+# git commit -m "mensagem"
+
+# Atualizar seu repositorio local em relacao ao repositorio remoto.
+# git pull
+
+# Envia seu codigo para o github
+# git push
